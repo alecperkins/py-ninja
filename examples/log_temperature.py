f342af4f96fa36f41f11508adecdefc14e5151f9
Alec Perkins <alecperkins@gmail.com>
2012-10-04 00:02:35 -0400

    Add basic temperature logging example.

---
+++ examples/log_temperature.py
@@ -0,0 +1,42 @@
+"""
+An example showing how to keep a running log of a temperature sensor's readings
+in a CSV file. Note: uses the Device.pulse method, which allows for watching
+only one device at a time.
+"""
+
+from ninja.api      import NinjaAPI
+from ninja.devices  import TemperatureSensor
+
+from datetime       import datetime
+
+
+
+# Set up the NinjaAPI and Device wrappers:
+
+# Access token from https://a.ninja.is/you#apiTab
+api         = NinjaAPI('<YOUR_ACCESS_TOKEN>')
+
+# Device GUID can be found using https://api.ninja.is/rest/v0/devices/?access_token=<YOUR_ACCESS_TOKEN>
+device      = TemperatureSensor(api, '<DEVICE_GUID>')
+
+
+
+# Prep the logfile with some headings
+LOGFILE = 'temp_in_celsius.csv'
+file(LOGFILE, 'w').write('Date,Temperature (C)\n')
+
+
+
+# Log the current temperature reading to a file, adding each
+# new entry of date and temperature in celsius to a new line.
+def saveTempCelsius(inst, data):
+    date = inst.last_read.isoformat()
+    line = date + ',' + str(data.c) + '\n'
+    print line[:-2]
+    file(LOGFILE, 'a').write(line)
+
+# Update the log on the heartbeat event.
+device.onHeartbeat(saveTempCelsius)
+
+# Run the heartbeat every 30 seconds.
+device.pulse(period=30)
