8e4833c50b46d8b2f9604fcddcbd5258565ce185
Alec Perkins <alecperkins@gmail.com>
2012-10-04 20:51:32 -0400

    Update Watcher example to use HumiditySensor instead of second TemperatureSensor

--- examples/multiple_devices_with_watcher.py
+++ examples/multiple_devices_with_watcher.py
@@ -4,7 +4,7 @@ process.
 """
 
 from ninja.api      import NinjaAPI, Watcher
-from ninja.devices  import TemperatureSensor
+from ninja.devices  import TemperatureSensor, HumiditySensor
 
 from datetime       import datetime
 
@@ -17,20 +17,33 @@ api         = NinjaAPI('<YOUR_ACCESS_TOKEN>')
 
 # Device GUID can be found using https://api.ninja.is/rest/v0/devices/?access_token=<YOUR_ACCESS_TOKEN>
 device1     = TemperatureSensor(api, '<DEVICE_1_GUID>')
-device2     = TemperatureSensor(api, '<DEVICE_2_GUID>')
+device2     = HumiditySensor(api, '<DEVICE_2_GUID>')
 
 # The watcher will provide a single loop for polling all of the devices.
 watcher     = Watcher()
 
 
 # Output the temperature to stdio.
-def printTempCelsius(inst, data):
+def printTempCelsius(inst, temperature):
     date = inst.last_read.isoformat()
-    print date + ' - ' + inst.guid + ': ' + str(data.c)
+    print '{date} - {id}: {temperature} C'.format(
+        date=date,
+        id=inst.guid,
+        temperature=temperature.c,
+    )
+
+def printRelHumidity(inst, humidity):
+    date = inst.last_read.isoformat()
+    print '{date} - {id}: {humidity}%'.format(
+        date=date,
+        id=inst.guid,
+        humidity=humidity,
+    )
+
 
 # Bind the output to the heartbeat event.
 device1.onHeartbeat(printTempCelsius)
-device2.onHeartbeat(printTempCelsius)
+device2.onHeartbeat(printRelHumidity)
 
 # Watch both devices in the same loop, triggering their heartbeats ever
 # 10 seconds.
