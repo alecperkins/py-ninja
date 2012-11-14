82a05a46fb11e08032ecc0c4b0a74800f2f1767c
Alec Perkins <alecperkins@gmail.com>
2012-11-14 02:09:57 -0500

    Add web_app.py example.

--- examples/README.md
+++ examples/README.md
@@ -6,5 +6,8 @@
 * `log_temperature.py` - recording a single device's data to a file
 * `multiple_devices_using_watcher.py` - multiple devices printing data, using a Watcher
 * `multiple_devices_with_nodes.py` - complex data flow with multiple devices, using Nodes
+* `web_app.py` - a sample web app (requires Flask) that lists devices and data
 
 To run, install `py-ninja`, or at least the requirements since the examples will look in the project directory for the `ninja` package, and add keys and IDs to a `secrets.py` file in the `/examples` directory. Then, `python <example_file>` will run the examples.
+
+Note: the `web_app.py` example also requires [Flask](http://flask.pocoo.org/) (`pip install flask`).
---
+++ examples/web_app.py
@@ -0,0 +1,134 @@
+"""
+An example web app that displays a list of devices, with links to a page for
+each device's data. The device page updates every 2 seconds with the latest
+heartbeat.
+
+Note: requires Flask
+"""
+
+from _examples import *
+
+from flask import Flask
+
+from ninja.api import NinjaAPI
+
+import json
+
+api = NinjaAPI(secrets.ACCESS_TOKEN)
+app = Flask(__name__)
+
+device_cache = {}
+
+BASE_HTML = """
+    <link rel="icon" href="">
+    <style>
+        table {
+            font-family: monospace;
+        }
+        td.label {
+            padding-right: 1em;
+            font-style: italic;
+        }
+        body {
+            max-width: 600px;
+            margin: 0 auto;
+            padding-top: 1em;
+        }
+    </style>
+    <script src="//cdnjs.cloudflare.com/ajax/libs/zepto/1.0rc1/zepto.min.js"></script>
+"""
+
+def getDevice(guid):
+    if not guid in device_cache:
+        device = api.getDevice(guid)
+        device_cache[guid] = device
+    else:
+        device = device_cache[guid]
+    return device
+
+
+@app.route('/')
+def hello():
+    user = api.getUser()
+    devices = api.getDevices()
+    html = BASE_HTML
+
+    html += """
+        <h1>Devices for {user.name}</h1>
+        <p>User ID: ({user.id})</p>
+    """.format(user=user)
+
+    html += """
+        <table>
+        <thead>
+            <tr>
+                <th>Device</th>
+                <th>ID</th>
+            </tr>
+        </thead>
+        <tbody>
+        """
+
+    for device in devices:
+        device_cache[device.guid] = device
+        html += """
+            <tr>
+                <td class="label">{device}</td>
+                <td><a href="/{device.guid}/">{device.guid} &raquo;</a></td>
+            </tr>
+        """.format(device=device)
+    html += '</tbody></table>'
+    return html
+
+@app.route('/<guid>/')
+def showDevice(guid):
+    device = getDevice(guid)
+
+    html = BASE_HTML
+
+    html += """
+        <h1>{device}</h1>
+        <a href="/">&laquo; index</a>        
+        <p>Device heartbeat every 2 seconds [<a href="heartbeat.json">raw</a>]</p>
+        <table>
+    """.format(device=device)
+
+    for field in device.asDict():
+        html += "<tr><td class='label'>%s</td><td>&hellip;</td></tr>" % (field,)
+
+    html += '</table>'
+
+    html += """
+        <script>
+            var $table = $('table');
+
+            function fetch(){
+                $.getJSON('heartbeat.json', function(response){
+                    render(response);
+                });
+            }
+
+            function render(device){
+                var html = '';
+                for(k in device){
+                    html += '<tr><td class="label">' + k + '</td><td>' + device[k] + '</td></tr>';
+                }
+                $table.html(html);
+            }
+            setInterval(fetch, 2000);
+        </script>
+    """
+    return html
+
+import json
+@app.route('/<guid>/heartbeat.json')
+def deviceHeartbeat(guid):
+    device = getDevice(guid)
+    device.heartbeat()
+    return json.dumps(device.asDict(for_json=True))
+
+
+
+app.run(debug=True)
+
+
